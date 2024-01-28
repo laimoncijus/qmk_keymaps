3523a4829fd789c15a403760951594a6bc9f17f9
nobwyn <8455481+nobwyn@users.noreply.github.com>
2024-01-28 21:12:24 +0100

    feat: switch to WIN key for TAP-nav

--- keymaps/custom/keymap.c
+++ keymaps/custom/keymap.c
@@ -5,7 +5,7 @@
 #define _FL 1
 
 /* custom keycodes */
-#define CTL_TAP LCTL_T(KC_F13)
+#define CTL_TAP LGUI_T(KC_F13)
 
 #define CTL_TAP_TIMEOUT 1000000 // 1 sec.
 
@@ -22,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_ENT,   KC_P7,    KC_P8,    KC_P9,    KC_PPLS,
   KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_NUHS,            KC_P4,    KC_P5,    KC_P6,
   KC_LSFT,  KC_NUBS,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,  KC_UP,    KC_P1,    KC_P2,    KC_P3,    KC_PENT,
-  CTL_TAP,  KC_LGUI,  KC_LALT,                      KC_SPC,                                 KC_RALT,  MO(_FL),  KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_P0,    KC_PDOT),
+  KC_LCTL,  CTL_TAP,  KC_LALT,                      KC_SPC,                                 KC_RALT,  MO(_FL),  KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_P0,    KC_PDOT),
 
 [_FL] = LAYOUT( // functions layer:
 // ESC   |  F1     |  F2     |  F3     |  F4     |  F5     |  F6     |  F7     |  F8     |  F9     |  F10    |  F11    |  F12    |  PSCR   |  DEL    |  INS    |  PGUP   |  PGDN
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // LSFT  |  <      |  y      |  x      |  c      |  v      |  b      |  n      |  m      |  ,      |  .      |  -      |  RSFT   |  UP     |  1      |  2      |  3      |  ENT
   _______,  _______,  RGB_M_P,  RGB_M_B,  RGB_M_R,  RGB_M_SW, _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_PGUP,  _______,  _______,  _______,  _______,
 // LCTL  |  WIN    |  LALT   |                      SPC                                  |  RALT   |  FN     |  RCTL   |  LEFT   |  DOWN   |  RGHT   |  0      |  ,      |
-  _______,  UC_WIN,   _______,                      _______,                                _______,  _______,  _______,  KC_HOME,  KC_PGDN,  KC_END ,  _______,  _______)
+  _______,  _______,  _______,                      _______,                                _______,  _______,  _______,  KC_HOME,  KC_PGDN,  KC_END ,  _______,  _______)
 };
 
 /* customizing function */
